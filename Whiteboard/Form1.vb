b1831aa3eea6a109b7b16c4e58e039da5996ee97
Morphy90 <42584110+Morphy90@users.noreply.github.com>
2018-11-16 15:23:06 -0600

    Add files via upload

--- Whiteboard/Form1.vb
+++ Whiteboard/Form1.vb
@@ -1,4 +1,14 @@
-﻿Public Class Login
+﻿Imports System.IO
+Imports System.Data.SQLite
+
+Public Class Login
+    Private connectionString As String = String.Empty
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connectionString = String.Format("Data Source={0};Version=3;",
+                                        Directory.GetCurrentDirectory() & "\" & "LMS.db")
+    End Sub
+
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
@@ -9,24 +19,47 @@
 
 
     Private Sub LoginBttn_Click(sender As Object, e As EventArgs) Handles LoginBttn.Click
-        If (Usr.Text = "student" And Psswrd.Text = "password") Then
+        If IsAuthenticated(Usr.Text.Trim, Psswrd.Text.Trim) Then
             Student.Show()
             Me.Hide()
             Usr.Clear()
             Psswrd.Clear()
             Usr.Select()
-
-        ElseIf (Usr.Text = "admin" And Psswrd.Text = "password") Then
-            Admin.Show()
-            Me.Hide()
-            Usr.Clear()
-            Psswrd.Clear()
-            Usr.Select()
-
         Else
-            MessageBox.Show("Invalid Username or Password")
+            MsgBox("Incorrect username/password")
         End If
+
     End Sub
+
+    Private Function IsAuthenticated(username As String, password As String) As Boolean
+        Dim IsValidUser As Boolean = False
+        Dim userInfo As DataRow = Nothing
+        Dim query As String = "SELECT * FROM student WHERE sUsername = @username"
+
+        Try
+            Using conn As New SQLiteConnection(connectionString.ToString)
+                Using cmd As New SQLiteCommand(conn)
+                    cmd.Parameters.AddWithValue("@username", username)
+                    cmd.CommandText = query
+                    conn.Open()
+                    Using da As New SQLiteDataAdapter(cmd)
+                        Dim dt As New DataTable
+                        da.Fill(dt)
+                        If dt.Rows.Count > 0 Then
+                            userInfo = dt.Rows(0)
+                            If userInfo("sPass").Equals(password) Then
+                                IsValidUser = True
+                            End If
+                        End If
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+
+        End Try
+        Return IsValidUser
+    End Function
+
 
     Private Sub Usr_TextChanged(sender As Object, e As EventArgs) Handles Usr.TextChanged
         Usr.Focus()
@@ -37,4 +70,6 @@
         LoginBttn.DialogResult = System.Windows.Forms.DialogResult.OK
 
     End Sub
+
+
 End Class
