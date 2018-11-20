7cd471f54fd92bdd7b2299afcedcc08ddf489247
Morphy90 <42584110+Morphy90@users.noreply.github.com>
2018-11-20 13:53:32 -0600

    Add files via upload

---
+++ Whiteboard/Whiteboard/Form3.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Admin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tabControl1 = New System.Windows.Forms.TabControl()
+        Me.addTab = New System.Windows.Forms.TabPage()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.editTab = New System.Windows.Forms.TabPage()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tabControl1.SuspendLayout()
+        Me.addTab.SuspendLayout()
+        Me.editTab.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tabControl1
+        '
+        Me.tabControl1.AccessibleName = ""
+        Me.tabControl1.Controls.Add(Me.addTab)
+        Me.tabControl1.Controls.Add(Me.editTab)
+        Me.tabControl1.Controls.Add(Me.TabPage1)
+        Me.tabControl1.Location = New System.Drawing.Point(1, 40)
+        Me.tabControl1.Name = "tabControl1"
+        Me.tabControl1.SelectedIndex = 0
+        Me.tabControl1.Size = New System.Drawing.Size(805, 464)
+        Me.tabControl1.TabIndex = 2
+        '
+        'addTab
+        '
+        Me.addTab.BackgroundImage = Global.Whiteboard.My.Resources.Resources.inBox_BG
+        Me.addTab.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.addTab.Controls.Add(Me.Label3)
+        Me.addTab.Controls.Add(Me.Label2)
+        Me.addTab.Location = New System.Drawing.Point(4, 22)
+        Me.addTab.Name = "addTab"
+        Me.addTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.addTab.Size = New System.Drawing.Size(797, 438)
+        Me.addTab.TabIndex = 1
+        Me.addTab.Text = "Home"
+        Me.addTab.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(3, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Grades Pending"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(149, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Enrollment Pending"
+        '
+        'editTab
+        '
+        Me.editTab.BackgroundImage = Global.Whiteboard.My.Resources.Resources.inBox_BG
+        Me.editTab.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.editTab.Controls.Add(Me.Button2)
+        Me.editTab.Controls.Add(Me.ComboBox1)
+        Me.editTab.Location = New System.Drawing.Point(4, 22)
+        Me.editTab.Name = "editTab"
+        Me.editTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.editTab.Size = New System.Drawing.Size(797, 438)
+        Me.editTab.TabIndex = 2
+        Me.editTab.Text = "Add Courses"
+        Me.editTab.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(7, 407)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Finish Enrollment"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 6)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "Select Student"
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackgroundImage = Global.Whiteboard.My.Resources.Resources.inBox_BG
+        Me.TabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.TabPage1.Controls.Add(Me.Button4)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.ComboBox2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(797, 438)
+        Me.TabPage1.TabIndex = 3
+        Me.TabPage1.Text = "Edit Grades"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(88, 407)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Clear"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(7, 407)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(6, 6)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 1
+        Me.ComboBox2.Text = "Select Student"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Yu Gothic UI", 16.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Label1.Location = New System.Drawing.Point(6, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 30)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "WhiteBoard"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(711, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Logout"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Admin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Whiteboard.My.Resources.Resources.login_BG
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(807, 504)
+        Me.Controls.Add(Me.tabControl1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Admin"
+        Me.Text = "Form3"
+        Me.tabControl1.ResumeLayout(False)
+        Me.addTab.ResumeLayout(False)
+        Me.addTab.PerformLayout()
+        Me.editTab.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tabControl1 As TabControl
+    Friend WithEvents addTab As TabPage
+    Friend WithEvents editTab As TabPage
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Whiteboard/Whiteboard/Resources/CreateDB.vb
@@ -0,0 +1,5 @@
+﻿Module CreateDB
+
+
+
+End Module
