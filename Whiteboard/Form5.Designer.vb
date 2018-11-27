ccce149a44165c93e3fabc4fe4afe307792350c7
Morphy90 <42584110+Morphy90@users.noreply.github.com>
2018-11-26 22:31:59 -0600

    Add files via upload

--- Whiteboard/Form5.Designer.vb
+++ Whiteboard/Form5.Designer.vb
@@ -23,16 +23,149 @@ Partial Class Form5
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TabControl2 = New System.Windows.Forms.TabControl()
+        Me.assignmentsTab = New System.Windows.Forms.TabPage()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.gradesTab = New System.Windows.Forms.TabPage()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TabControl2.SuspendLayout()
+        Me.assignmentsTab.SuspendLayout()
+        Me.gradesTab.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(1001, 37)
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(38, 156)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(131, 46)
+        Me.Button1.Size = New System.Drawing.Size(229, 72)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Return"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "Home"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(44, 250)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(223, 112)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(44, 373)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(223, 112)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(44, 496)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(223, 112)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(44, 621)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(223, 112)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Label5"
+        '
+        'TabControl2
+        '
+        Me.TabControl2.Controls.Add(Me.assignmentsTab)
+        Me.TabControl2.Controls.Add(Me.gradesTab)
+        Me.TabControl2.Location = New System.Drawing.Point(284, 126)
+        Me.TabControl2.Name = "TabControl2"
+        Me.TabControl2.SelectedIndex = 0
+        Me.TabControl2.Size = New System.Drawing.Size(893, 607)
+        Me.TabControl2.TabIndex = 6
+        '
+        'assignmentsTab
+        '
+        Me.assignmentsTab.Controls.Add(Me.Label8)
+        Me.assignmentsTab.Location = New System.Drawing.Point(4, 29)
+        Me.assignmentsTab.Name = "assignmentsTab"
+        Me.assignmentsTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.assignmentsTab.Size = New System.Drawing.Size(885, 574)
+        Me.assignmentsTab.TabIndex = 0
+        Me.assignmentsTab.Text = "Assignments"
+        Me.assignmentsTab.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(3, 3)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(235, 91)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Assignments"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'gradesTab
+        '
+        Me.gradesTab.Controls.Add(Me.Label7)
+        Me.gradesTab.Controls.Add(Me.Label6)
+        Me.gradesTab.Location = New System.Drawing.Point(4, 29)
+        Me.gradesTab.Name = "gradesTab"
+        Me.gradesTab.Padding = New System.Windows.Forms.Padding(3)
+        Me.gradesTab.Size = New System.Drawing.Size(885, 574)
+        Me.gradesTab.TabIndex = 1
+        Me.gradesTab.Text = "Grades"
+        Me.gradesTab.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(281, 27)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(196, 46)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "- / -"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label7.UseCompatibleTextRendering = True
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(6, 27)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(269, 46)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Total:"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Form5
         '
@@ -41,12 +174,33 @@ Partial Class Form5
         Me.BackgroundImage = Global.Whiteboard.My.Resources.Resources.Student_BG
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1210, 775)
+        Me.Controls.Add(Me.TabControl2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form5"
         Me.Text = "Form5"
+        Me.TabControl2.ResumeLayout(False)
+        Me.assignmentsTab.ResumeLayout(False)
+        Me.gradesTab.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TabControl2 As TabControl
+    Friend WithEvents assignmentsTab As TabPage
+    Friend WithEvents gradesTab As TabPage
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
 End Class
--- Whiteboard/Form5.vb
+++ Whiteboard/Form5.vb
@@ -1,10 +1,65 @@
-﻿Public Class Form5
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+﻿Imports System.Data.SQLite
+Imports System.IO
+
+Public Class Form5
+    Private db As New DBConnection
+    Dim MyLabel() As Label
+
+    Public Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DynamicLabels()
+        If db.HasConnection() Then
+            If db.SQLDS IsNot Nothing Then
+                db.SQLDS.Clear()
+            End If
+            db.RunQuery("SELECT c.courseSubj AS sub, c.courseNum AS cn, r.className AS class, t.tName AS teacher, r.term, r.days, r.location
+                        FROM course c, classroom r, teacher t
+                        WHERE c.courseId=r.course_id AND t.teacherId=r.teacher_id AND r.classId='" & Student.classId & "'")
+            Label1.Text = String.Format("" & CType(db.SQLDS.Tables(0).Rows(0).Item("sub"), String) & " " & CType(db.SQLDS.Tables(0).Rows(0).Item("cn"), String) & " " & CType(db.SQLDS.Tables(0).Rows(0).Item("class"), String))
+            Label2.Text = String.Format("Professor:" & Environment.NewLine & CType(db.SQLDS.Tables(0).Rows(0).Item("teacher"), String))
+            Label3.Text = String.Format("Term: " & Environment.NewLine & CType(db.SQLDS.Tables(0).Rows(0).Item("term"), String))
+            Label4.Text = String.Format("Days: " & Environment.NewLine & CType(db.SQLDS.Tables(0).Rows(0).Item("days"), String))
+            Label5.Text = String.Format("Location: " & Environment.NewLine & CType(db.SQLDS.Tables(0).Rows(0).Item("location"), String))
+        End If
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Student.Show()
+        Me.Controls.Clear()
+        InitializeComponent()
         Me.Hide()
     End Sub
+
+    Sub DynamicLabels()
+        Dim i As Integer
+        Dim x As Integer = 300
+        Dim y As Integer = 200
+        Dim tp As TabPage = TabControl2.TabPages(1)
+        If db.HasConnection() Then
+            If db.SQLDS IsNot Nothing Then
+                db.SQLDS.Clear()
+            End If
+            db.RunQuery("SELECT g.examName AS exam, g.examScore score
+                        From student s, grades g, classroom r
+                        Where s.studentId=g.student_id AND r.classId=g.classroom_id AND s.sUsername='" & Login.Usr.Text & "' AND r.classId ='" & Student.classId & "' ")
+            For i = 0 To db.SQLDS.Tables(0).Rows.Count - 1
+                ReDim MyLabel(db.SQLDS.Tables(0).Rows.Count)
+                y += 50
+                With MyLabel(i)
+                    MyLabel(i) = New Label()
+                    MyLabel(i).Name = i.ToString
+                    MyLabel(i).Location = New Point(x, y)
+                    MyLabel(i).Size = New Size(700, 40)
+                    MyLabel(i).Font = New Font("Microsoft Sans Serif", 14)
+                    MyLabel(i).Text = String.Format(CType(db.SQLDS.Tables(0).Rows(i).Item("exam"), String) & "      " & CType(db.SQLDS.Tables(0).Rows(i).Item("score"), String))
+                End With
+                tp.Controls.Add(MyLabel(i))
+            Next
+        End If
+    End Sub
+
+    Public Sub go()
+        Me.Form5_Load(Me, Nothing)
+    End Sub
+
 End Class
